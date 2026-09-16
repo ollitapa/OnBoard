0ba6305b24a8ff9d9ee38094b22610e6f3a7a0e9
Olli Tapaninen <olli.tapaninen@icloud.com>
2026-09-16 10:51:35 +0200

    Remove redundant tests

--- OnBoardUITests/OnBoardUITests.swift
+++ OnBoardUITests/OnBoardUITests.swift
@@ -10,16 +10,11 @@ import XCTest
 final class OnBoardUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     @MainActor
@@ -39,13 +34,5 @@ final class OnBoardUITests: XCTestCase {
                      "Expected the first mock stop to appear in the nearby list.")
         XCTAssertTrue(marketSquare.exists,
                      "Expected the second mock stop to appear in the nearby list.")
-    }
-
-    @MainActor
-    func testLaunchPerformance() throws {
-        // This measures how long it takes to launch your application.
-        measure(metrics: [XCTApplicationLaunchMetric()]) {
-            XCUIApplication().launch()
-        }
     }
 }
